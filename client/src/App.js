fc51f8b6de9ddfa6f6d79f03e74ad60b6c3d1fe8
Sanam_App <ugyendechen00@gmail.com>
2023-06-17 01:19:11 +0600

    app

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,12 @@ import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
-import AppForm from "./components/appForm";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/services" element={<Services />} />
-      <Route path="/incubationapp" element={<AppForm />} />
     </Routes>
   );
 };
