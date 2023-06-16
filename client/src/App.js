e1db800099b9c7dffcc884e987c01eb93763fe8b
knthinley <ryoutamikasa@gmail.com>
2023-06-16 11:53:53 +0600

    startup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+const App = () => {
+  return (
+    <Routes>
+      <div>hello</div>
+    </Routes>
+  );
+};
+
+export default App;
