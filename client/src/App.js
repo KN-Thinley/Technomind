98a966dd03b78d569d299270abe7c114ddecd9b2
Sanam_App <ugyendechen00@gmail.com>
2023-06-17 03:44:21 +0600

    appliform

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,18 @@ import { Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import AdminDashboard from "./components/adminComponents/AdminDashboard";
+import AppForm from "./components/AppliForm";
+import AddInfo from "./components/AddInfo";
+import ProInfo from "./components/ProInfo";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/services" element={<Services />} />
+      <Route path="/appform" element={<AppForm />} />
+      <Route path="/additional" element={<AddInfo />} />
+      <Route path="/proposed" element={<ProInfo />} />
 
       {/* ADMIN ROUTES */}
       <Route path="/admin" element={<AdminDashboard />} />
