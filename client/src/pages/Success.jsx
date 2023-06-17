9e0f4283bd3f04abb2a07ffda556c4c7a60073f0
Kinzang Dorji <kinzasdorji66@gmail.com>
2023-06-17 09:15:16 +0600

    now

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -15,7 +15,7 @@ const Success = () => {
     return () => {
       clearTimeout(timeout);
     };
-  }, [navigate]);
+  }, [navigate]);   
 
   const fade = useSpring({ opacity: 1, from: { opacity: 0 } });
 
@@ -23,7 +23,7 @@ const Success = () => {
     <animated.div className="success-page" style={fade}>
       <div className="flex items-center justify-center h-screen">
       <div className="bg-green-200 border border-green-500 p-4 text-center">
-        <div className="w-20 h-20 bg-green-500 rounded-full mx-auto mb-2 flex items-center justify-center"><img src={tickImg} alt="img" /></div>
+        <div className="w-20 h-20 bg-green-500 rounded-full mx-auto mb-2 flex items-center justify-center"><img src={tickImg} alt="img" className="rounded-full"/></div>
         <h2 className="text-base">Your submission was successful.</h2>
         <p>Thank you for your participation.</p>
       </div>
