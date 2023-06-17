4584401d0ec6d3315ff9442aae7a99847a854753
Kinzang Dorji <kinzasdorji66@gmail.com>
2023-06-17 09:15:38 +0600

    now

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,13 @@ import Login from "./pages/Login";
 import About from "./pages/About";
 import Design from "./components/Design";
 
-import Incubation from "./pages/Incubation";
+import AppliForm from "./components/AppliForm"
 import AddInfo from "./components/AddInfo";
 
 import Register from "./pages/Register";
 import MenSignup from "./pages/MenSignup";
+import Success from "./pages/Success";
+import ProInfo from "./components/ProInfo";
 
 const App = () => {
   return (
@@ -26,13 +28,13 @@ const App = () => {
       <Route path="/mentorSignup" element={<MenSignup />} />
       <Route path="/menteeSignup" element={<MenSignup />} />
       <Route path="/success" element={<Success />} />
+      <Route path="/services/incubation" element={<AppliForm/>}/>
 
       {/* ADMIN ROUTES */}
       <Route path="/admin" element={<AdminHome />} />
-      <Route path="/services/incubation" element={<AppForm />} />
       <Route path="/testimonials" element={<Testimonials />} />
-      <Route path="/proinfo" element={<ProInfo />} />
-      <Route path="/addinfo" element={<AddInfo />} />
+      <Route path="/services/incubation/final" element={<ProInfo />} />
+      <Route path="/services/incubation/additional" element={<AddInfo />} />
     </Routes>
   );
 };
