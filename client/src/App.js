f0702565da3a760ca3e890d933dde85578a18947
Sanam_App <ugyendechen00@gmail.com>
2023-06-17 07:20:46 +0600

    partical

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,21 @@ import Services from "./pages/Services";
 import Testimonials from "./pages/Testimonials";
 import Login from "./pages/Login";
 import About from "./pages/About";
+import Design from "./components/Design";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/services/incubation" element={<Services />} />
-      <Route path="/testimonials" element={<Testimonials />} />
-    </Routes>
+    <div style={{ position: "relative" }}>
+      <Design />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/services/incubation" element={<Services />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+      </Routes>
+    </div>
   );
 };
 
