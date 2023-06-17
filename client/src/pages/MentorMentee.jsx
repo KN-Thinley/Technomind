d7499597f006e56c99449fa8cb2fb106bfe2cda1
Glen-Gal <Jamphelyigzins15@gmail.com>
2023-06-17 14:16:26 +0600

    Hello

--- client/src/pages/MentorMentee.jsx
+++ client/src/pages/MentorMentee.jsx
@@ -1,22 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function ProfilePage() {
-  const [userData, setUserData] = useState(null);
   const [mentors, setMentors] = useState([]);
   const [mentees, setMentees] = useState([]);
 
   useEffect(() => {
-    // Fetch current user data
-    axios
-      .get("/user/getUser")
-      .then((response) => {
-        setUserData(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching user data:", error);
-      });
-
     // Fetch available mentors
     axios
       .get("/user/getMentors")
@@ -38,41 +29,61 @@ function ProfilePage() {
       });
   }, []);
 
-  if (!userData) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div>
-      <h1>Welcome, {userData.name}</h1>
-      <h2>Your Profile</h2>
-      {/* Render user profile data */}
-      <div>Name: {userData.name}</div>
-      <div>Contact: {userData.contact}</div>
-      <div>Bio: {userData.bio}</div>
-      <div>Field: {userData.field}</div>
-
-      <h2>Available Mentors</h2>
-      {/* Render available mentors */}
-      {mentors.map((mentor) => (
-        <div key={mentor._id}>
-          <div>Name: {mentor.name}</div>
-          <div>Contact: {mentor.contact}</div>
-          <div>Bio: {mentor.bio}</div>
-          <div>Field: {mentor.field}</div>
+      {/* Navbar */}
+      <Navbar />
+      {/* Hero Banner */}
+      <div className="flex items-center justify-end w-screen h-screen heroBanner bg-[url('./assets/heroBanner.png')] bg-cover bg-no-repeat text-white	">
+        <div className="text-center mr-auto lg:ml-40 md:ml-20 ml-10">
+          <div
+            className="text-7xl animate-bottom"
+            style={{ textAlign: "left" }}
+          >
+            Mentorship
+          </div>
         </div>
-      ))}
+        {/* <div>
+          <button
+            className="fixed bottom-4 right-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={openChatbot}
+          ></button>
+        </div> */}
+      </div>
 
-      <h2>Available Mentees</h2>
-      {/* Render available mentees */}
-      {mentees.map((mentee) => (
-        <div key={mentee._id}>
-          <div>Name: {mentee.name}</div>
-          <div>Contact: {mentee.contact}</div>
-          <div>Bio: {mentee.bio}</div>
-          <div>Field: {mentee.field}</div>
+      <div className="section flex w-full justify-around align-middle m-7 relative">
+        <div className="userProfile w-1/4 flex flex-col justify-center align-middle p-5 text-center rounded-lg bg-slate-300 gap-4	">
+          <h2 className="text-xl">Your Mentee Profile</h2>
+          {/* Render user profile data */}
+          <div
+            className="profilePic w-24 h-24 bg-black rounded-full m-auto my-5
+"
+          >
+            <img src="#" alt="" />
+          </div>
+          <div>John Doe</div>
+          <div>Contact: 123123</div>
+          <div>"I love Pineapples"</div>
+          <div>Field: Business</div>
         </div>
-      ))}
+
+        <button className="absolute connect">Connect</button>
+
+        <div className="userProfile w-1/4 flex flex-col justify-center align-middle p-5 text-center rounded-lg bg-slate-300 gap-4	">
+          <h2 className="text-xl">Available Mentors</h2>
+          {/* Render user profile data */}
+          <div className="profilePic w-24 h-24 bg-black rounded-full m-auto my-5">
+            <img src="#" alt="" />
+          </div>
+          <div>John Doe</div>
+          <div>Contact: 123123</div>
+          <div>"I love Pineapples"</div>
+          <div>Field: Business</div>
+        </div>
+      </div>
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
